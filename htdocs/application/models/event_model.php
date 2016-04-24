be4ed4b1951c1bf9e54e3ef47ffe27aa7523f0e7
fedorareis <fedorareis@gmail.com>
2016-04-24 13:51:59 -0700

    Fixed the slider on the home page and cleaned up the staff page

--- htdocs/application/models/event_model.php
+++ htdocs/application/models/event_model.php
@@ -8,23 +8,32 @@ class Event_model extends CI_Model
     /**
      * Function to return event informtion
      */
-    function getEvents()
+    /*function getEvents()
     {
         $result = $this->db->select('*')->from('events')->order_by('date')->get();
         return $result->result();
+    }*/
+
+    function getEvents() {
+        $response = http_get("http://androidcru-prod.jonamireh.com/api/events");
+        $body = http_parse_message($response)->body;
+        // return $body;
+        // print_r(json_decode($body));
+        return json_decode($body);
     }
-    
+
+
     function saveEvent($event)
     {
         $this->db->insert('events',$event);
         $this->db->insert_id()?'yes':'no';
     }
-    
+
     function deleteEvent($id)
     {
-        $this->db->delete('events', array('Id' => $id)); 
+        $this->db->delete('events', array('Id' => $id));
     }
-    
+
     function updateEvent($id, $event) {
         $this->db->where('Id', $id);
         $this->db->update('events', $event);
