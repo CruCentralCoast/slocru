c4bf44a3bafd14c39d4e9d379ecbf79814f783bb
fedorareis <fedorareis@gmail.com>
2016-09-07 22:04:10 -0700

    Cleaning up files

--- rides/ContactForm_OriginalFiles/README-ContactForm.txt
+++
@@ -1,5 +0,0 @@
-Contact form - as is, creates a email contact form
-
-Use for slo rides to church:
-Turn form into php submit to add driver to database
-use email scripts to send confirmation to drivers/riders
--- rides/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="privacy" style="float:left; margin-left:10px; font-size:10pt;">
-<a href="/rides/about/">Privacy Info</a>
-</div>
-<div id="footer">
-questions or comments? send them to <a href="mailto:theslocru@gmail.com">theslocru@gmail.com</a>
-</div>
