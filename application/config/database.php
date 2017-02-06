2a3a4a6eba8de43e151bee00086d7db4527b7649
fedorareis <fedorareis@gmail.com>
2017-02-06 01:38:30 -0800

    Trying to fix db issue

--- application/config/database.php
+++ application/config/database.php
@@ -45,7 +45,24 @@
 | the active record class
 */
 
+$active_group = 'default';
+$active_record = TRUE;
 
+$db['default']['hostname'] = 'localhost';//localhost';//mysql3.000webhost.com';
+$db['default']['username'] = 'slocrus1_all';//a7253567_admin';
+$db['default']['password'] = '2cor129';//romans125';
+$db['default']['database'] = 'slocrus1_main';//a7253567_slocru';
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
