cb5de4fa5488c0ea50d51708ff446c9a847a8406
fedorareis <fedorareis@gmail.com>
2017-02-08 23:38:40 -0700

    started leadership framework

---
+++ application/views/leadership.php
@@ -0,0 +1,34 @@
+<div class="container top">
+    <div class="box">
+        <div class="text" id="housing">
+            <p class="applying" id="line1">APPLYING FOR LEADERSHIP</p>
+            <p class="applying" id="line2">We are so excited that you are considering leading with Cru Central Coast! Below, you will find a description of what leadership looks like and what different roles entail. Remember to look at the movement that you are involved in as that is where it makes the most sense for you to lead.</p>
+        </div>
+    </div>
+    <div class="picture">
+        <a href="construction"><p class="sermons">SERMONS & WEEKLY MEETINGS</p></a>
+    </div>
+</div>
+<div class="container bottom">
+    <div class="shift">
+        <div class="column resources" id="section1">
+            <p class="headings">RECOMMENDED SITES</p>
+            <div class="sites">
+                
+            </div>
+        </div>
+        <div class="column resources" id="section2">
+            <p class="headings">GUIDES</p>
+            <div class="guides">
+                <p class="subheadings">Prayer</p>
+                
+            </div>
+        </div>
+        <div class="column resources" id="section3">
+            <p class="headings">DOWNLOAD NOW</p>
+            <div class="downloads">
+                
+            </div>
+        </div>
+    </div>
+</div>
