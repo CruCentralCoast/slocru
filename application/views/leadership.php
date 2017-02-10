6f71fc43b69215886a7346fd7da54266e10d6c2f
fedorareis <fedorareis@gmail.com>
2017-02-10 15:10:44 -0700

    Leadership styling

--- application/views/leadership.php
+++ application/views/leadership.php
@@ -1,6 +1,7 @@
 <div class="container top">
     <div class="box">
         <div class="text" id="housing">
+            <p class="applying" id="line1">APPLYING FOR LEADERSHIP</p>
             <p class="applying" id="line2">We are so excited that you are considering leading with Cru Central Coast! Below, you will find a description of what leadership looks like and what different roles entail. Remember to look at the movement that you are involved in as that is where it makes the most sense for you to lead.</p>
         </div>
     </div>
@@ -11,20 +12,20 @@
 <div class="container bottom">
     <div class="shift">
         <div class="column resources" id="section1">
-            <p class="headings">RECOMMENDED SITES</p>
+            <p class="headings">LEADERSHIP ATTRIBUTES</p>
             <div class="sites">
-                
+                <p>As we select leaders for our different movements, we are looking for four things to be present in our applicants. These traits include being Faithful, a Leader, Available, and Teachable; or as we abbreviate it: FLAT. Here’s what we mean:</p>
             </div>
         </div>
         <div class="column resources" id="section2">
-            <p class="headings">GUIDES</p>
+            <p class="headings">LEADERSHIP APPLICATION</p>
             <div class="guides">
                 <p class="subheadings">Prayer</p>
                 
             </div>
         </div>
         <div class="column resources" id="section3">
-            <p class="headings">DOWNLOAD NOW</p>
+            <p class="headings">TEAM / COORDINATOR DESCRIPTIONS</p>
             <div class="downloads">
                 
             </div>
