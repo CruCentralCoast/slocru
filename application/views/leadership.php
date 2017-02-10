1ba570d57d2e9c1d1888a3dfac8a61fc7ad848f6
fedorareis <fedorareis@gmail.com>
2017-02-10 14:59:09 -0700

    Leadership styling

--- application/views/leadership.php
+++ application/views/leadership.php
@@ -1,12 +1,11 @@
 <div class="container top">
     <div class="box">
         <div class="text" id="housing">
-            <p class="applying" id="line1">APPLYING FOR LEADERSHIP</p>
             <p class="applying" id="line2">We are so excited that you are considering leading with Cru Central Coast! Below, you will find a description of what leadership looks like and what different roles entail. Remember to look at the movement that you are involved in as that is where it makes the most sense for you to lead.</p>
         </div>
     </div>
     <div class="picture">
-        <a href="construction"><p class="sermons">SERMONS & WEEKLY MEETINGS</p></a>
+        <a href="construction"><p class="sermons">APPLYING FOR LEADERSHIP</p></a>
     </div>
 </div>
 <div class="container bottom">
