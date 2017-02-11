f508010db9bf674319a5392703a441e32eccba98
fedorareis <fedorareis@gmail.com>
2017-02-10 22:33:56 -0500

    Leadership styling

--- application/views/leadership.php
+++ application/views/leadership.php
@@ -13,9 +13,8 @@
         <div class="column resources" id="section1">
             <p class="headings">LEADERSHIP ATTRIBUTES</p>
             <div class="sites">
-                <p>As we select leaders for our different movements, we are looking for four things to be present in our applicants.</p>
-                <p>These traits include being Faithful, a Leader, Available, and Teachable; or as we abbreviate it: FLAT.</p>
-                <p>Here's what we mean:</p>
+                <p>As we select leaders for our different movements, we are looking for four things to be present in our applicants. These traits include being Faithful, a Leader, Available, and Teachable; or as we abbreviate it: <span class="list">FLAT</span>.
+                <br />Here's what we mean:</p>
 
                 <p><span class="list">Faithful:</span> Being an active member in the movement you are applying for leadership with and committing to the responsibilities for the leadership position.</p>
                 <p><span class="list">Leader:</span> Being a person of influence with a willingness to focus on the needs of others.</p>
