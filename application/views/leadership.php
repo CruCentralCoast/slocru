b587d36aee2ba70301168fcf9d3509b779af395f
fedorareis <fedorareis@gmail.com>
2017-02-10 22:31:40 -0500

    Leadership styling

--- application/views/leadership.php
+++ application/views/leadership.php
@@ -13,7 +13,14 @@
         <div class="column resources" id="section1">
             <p class="headings">LEADERSHIP ATTRIBUTES</p>
             <div class="sites">
-                <p>As we select leaders for our different movements, we are looking for four things to be present in our applicants. These traits include being Faithful, a Leader, Available, and Teachable; or as we abbreviate it: FLAT. Here's what we mean:</p>
+                <p>As we select leaders for our different movements, we are looking for four things to be present in our applicants.</p>
+                <p>These traits include being Faithful, a Leader, Available, and Teachable; or as we abbreviate it: FLAT.</p>
+                <p>Here's what we mean:</p>
+
+                <p><span class="list">Faithful:</span> Being an active member in the movement you are applying for leadership with and committing to the responsibilities for the leadership position.</p>
+                <p><span class="list">Leader:</span> Being a person of influence with a willingness to focus on the needs of others.</p>
+                <p><span class="list">Available:</span> Willing to commit to being a part of a Community Group with Cru Central Coast as well as events, conferences, and leadership activities sponsored by your movement within Cru Central Coast.</p>
+                <p><span class="list">Teachable:</span> Having a desire to grow and a desire to receive input and feedback from others regarding your leadership.</p>
             </div>
         </div>
         <div class="column resources" id="section2">
